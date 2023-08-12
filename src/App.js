1e2ef52f940be323b83bb89b6a3224e50b6143b5
Najmiddin Nazirov <nnazirov@transoxiania.com>
2023-08-12 17:01:29 +0500

    clean code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { Route, Routes, useNavigate, Navigate } from "react-router-dom";
-import Projects from "./pages/Projects";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Login from "./components/Login";
-import Tabs from "./components/Project/Tabs";
 import { useEffect } from "react";
 import "./App.css";
 import { ToastContainer } from "react-toastify";
@@ -26,8 +24,6 @@ function App() {
           <ToastContainer />
           <Routes>
             <Route path="/" element={<Navigate to="/projects" />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/projects/:id/details" element={<Tabs />} />
             <Route path="/users" element={<Users />} />
           </Routes>
         </>
