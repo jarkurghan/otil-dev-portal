c9dcaaa7e25f9e951dc50c506cbcaf1093b3ec86
Najmiddin Nazirov <nnazirov@transoxiania.com>
2024-01-07 15:48:28 +0500

    user page and create-user page is ready

---
+++ src/pages/CreateUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateUser from "../components/User/CreateUser";
+
+const CreateUserPage = () => {
+    return (
+        <div className="container mx-auto">
+            <CreateUser />
+        </div>
+    );
+};
+
+export default CreateUserPage;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -5,12 +5,13 @@ import { useState } from "react";
 import Spinner from "react-spinner-material";
 import UsersTable from "../components/User/Users";
 import "../components/User/style.css";
-import CreateUser from "../components/User/CreateUser";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
-    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
     const [loading, setLoading] = useState(true);
     const [device] = useState(navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i));
+
     return (
         <div className="mx-5 mt-5">
             <UsersTable setLoading={setLoading} />
@@ -21,11 +22,8 @@ const Users = () => {
             ) : (
                 <>
                     <UserPolicies />
-                    <CreateUser open={open} setOpen={setOpen} />
-                    <div className="h-16 text-left">
-                        <button onClick={() => setOpen(true)} className="bg-indigo-300 hover:bg-indigo-400 text-gray-800 font-bold py-2 px-4 rounded my-4">
-                            Create User
-                        </button>
+                    <div className="h-16 text-left" onClick={() => navigate("/create-user")}>
+                        <button className="bg-indigo-300 hover:bg-indigo-400 text-gray-800 font-bold py-2 px-4 rounded my-4">Create User</button>
                     </div>
                 </>
             )}
